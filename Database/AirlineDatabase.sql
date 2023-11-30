05eaa990b6d239438dfd0e212d76829ff6b801d3
makmal21 <makmal@ucalgary.ca>
2023-11-30 02:17:13 -0700

    Added FlightID to Seat table and fixed seat # and type.

--- Database/AirlineDatabase.sql
+++ Database/AirlineDatabase.sql
@@ -83,41 +83,51 @@ CREATE TABLE Seat (
   PRIMARY KEY (SeatID),
   FOREIGN KEY (FlightID) REFERENCES FLIGHT(FlightID)
 );
--- need to add logic so price associates with seat type 
--- NEED TO FIX SEAT TABLE - check if price works and re-organize the seat type. 
--- start off with seats all avaliable and then maybe as we start having a purchase go
--- through then ticket table is populated? 
+
+
+
+-- eveything is avliable so that way once its picked it can be changed to unavaible ? 
 INSERT INTO Seat (SeatID, SeatNo, Type, Status, FlightID) VALUES
-('85f8667e-c', '39S', 'Comfort', 'Unavailable', 1),
-('96e1d19d-4', '20Q', 'Ordinary', 'Unavailable', 1),
-('c536d8c8-7', '11G', 'Ordinary', 'Unavailable', 1),
-('647abf05-0', '7R', 'Business Class', 'Available', 1),
-('a977311a-7', '45G', 'Comfort', 'Available', 1),
-('ee7bf488-e', '31V', 'Comfort', 'Available', 1),
-('dc55eb42-0', '15L', 'Ordinary', 'Unavailable', 1),
-('6c9c4c85-0', '40X', 'Business Class', 'Available', 1),
-('ac942807-8', '27Q', 'Ordinary', 'Available', 1),
-('5dd61f55-3', '27G', 'Business Class', 'Unavailable', 1),
-('98c6d610-1', '14D', 'Ordinary', 'Available', 1),
-('74a1ca2f-e', '10Q', 'Business Class', 'Unavailable', 1),
-('171a22e3-c', '26L', 'Ordinary', 'Available', 1),
-('ab1ebc23-b', '43Z', 'Business Class', 'Available', 1),
-('ab10fccf-f', '42R', 'Ordinary', 'Available', 1),
-('756d1b07-8', '19P', 'Ordinary', 'Unavailable', 1),
-('1aae4ced-9', '7J', 'Ordinary', 'Available',1 ),
-('050b09bf-c', '12A', 'Business Class', 'Available', 1),
-('ec7865ca-6', '20Z', 'Comfort', 'Available', 1),
-('93b95dd8-1', '13J', 'Business Class', 'Unavailable', 1),
-('b2793ce6-6', '5V', 'Comfort', 'Available', 1),
-('acf9375d-4', '49X', 'Ordinary', 'Available', 1),
-('146db653-b', '35P', 'Ordinary', 'Unavailable', 1),
-('1cf34ff5-b', '24Q', 'Comfort', 'Unavailable', 1),
-('861dfe5f-d', '15H', 'Comfort', 'Unavailable', 1),
-('b9be886f-9', '6T', 'Ordinary', 'Available',1),
-('42e17a29-7', '49C', 'Ordinary', 'Unavailable', 1),
-('486b30fe-1', '25H', 'Ordinary', 'Available', 1),
-('cba55c06-2', '44V', 'Comfort', 'Unavailable', 1),
-('0778072a-c', '36L', 'Business Class', 'Available',1 );
+('1AAA', '1', 'Business Class', 'Available', 1),
+('1AAB', '2', 'Business Class', 'Available', 1),
+('1AAC', '3', 'Business Class', 'Available', 1),
+('1AAD', '4', 'Business Class', 'Available', 1),
+('1AAE', '5', 'Comfort', 'Available', 1),
+('1AAF', '6', 'Comfort', 'Available', 1),
+('1AAG', '7', 'Comfort', 'Available', 1),
+('1AAH', '8', 'Comfort', 'Available', 1),
+('1AAI', '9', 'Comfort', 'Available', 1),
+('1AAJ', '10', 'Comfort', 'Available', 1),
+('1AAK', '11', 'Ordinary', 'Available', 1),
+('1AAL', '12', 'Ordinary', 'Available', 1),
+('1AAM', '13', 'Ordinary', 'Available', 1),
+('1AAN', '14', 'Ordinary', 'Available', 1),
+('1AAO', '15', 'Ordinary', 'Available', 1),
+('1AAP', '16', 'Ordinary', 'Available', 1),
+('1AAQ', '17', 'Ordinary', 'Available', 1),
+('1AAR', '18', 'Ordinary', 'Available', 1),
+('1AAS', '19', 'Ordinary', 'Available', 1),
+('1AAT', '20', 'Ordinary', 'Available', 1),
+('1AAU', '21', 'Business Class', 'Available', 1),
+('1AAV', '22', 'Business Class', 'Available', 1),
+('1AAW', '23', 'Business Class', 'Available', 1),
+('1AAX', '24', 'Business Class', 'Available', 1),
+('1AAY', '25', 'Comfort', 'Available', 1),
+('1AAZ', '26', 'Comfort', 'Available', 1),
+('2AAA', '27', 'Comfort', 'Available', 1),
+('2AAB', '28', 'Comfort', 'Available', 1),
+('2AAC', '29', 'Comfort', 'Available', 1),
+('2AAD', '30', 'Comfort', 'Available', 1),
+('2AAE', '31', 'Ordinary', 'Available', 1),
+('2AAF', '32', 'Ordinary', 'Available', 1),
+('2AAG', '33', 'Ordinary', 'Available', 1),
+('2AAH', '34', 'Ordinary', 'Available', 1),
+('2AAI', '35', 'Ordinary', 'Available', 1),
+('2AAJ', '36', 'Ordinary', 'Available', 1),
+('2AAK', '37', 'Ordinary', 'Available', 1),
+('2AAL', '38', 'Ordinary', 'Available', 1),
+('2AAM', '39', 'Ordinary', 'Available', 1),
+('2AAN', '40', 'Ordinary', 'Available', 1);
 
 -- Create Payment Table
 DROP TABLE IF EXISTS Payment;
@@ -216,21 +226,19 @@ CREATE TABLE Ticket (
 );
 
 INSERT INTO Ticket (TicketID, Name, Email, FlightID, PaymentID, SeatID) VALUES
-('o5pllb', 'Brandon Campbell', 'campbellbrandon@example.net', 23, '0d4aa4', '146db653-b'),
-('6wftsi', 'Jennifer Combs', 'combsjennifer@example.org', 24, 'ed9b4f', '1cf34ff5-b'),
-('2aobyz', 'Donald Trump', 'donald13@example.com', 27, '148482', '42e17a29-7'),
-('fvsoti', 'Kristen Dougherty', 'doughertykristen@example.org', 10, '02ea11', '5dd61f55-3'),
-('na6etk', 'Francis Young', 'fyoung@example.org', 12, '6d4305', '74a1ca2f-e'),
-('joer0v', 'Manuel Green', 'greenmanuel@example.org', 16, 'ceecd7', '756d1b07-8'),
-('mubu6e', 'Joshua Jones', 'jonesjoshua@example.org', 1, 'e5a8fa', '85f8667e-c'),
-('ohmynu', 'Lonnie Anderson', 'landerson@example.com', 25, 'c72f80', '861dfe5f-d'),
-('1b3f8x', 'Rhonda Livingston', 'livingstonrhonda@example.org', 20, '1fd3ee', '93b95dd8-1'),
-('2ibrzw', 'Ryan Gordon', 'rgordon@example.net', 2, 'cfe942', '96e1d19d-4'),
-('qtq1da', 'Ronald Jacobson', 'rjacobson@example.com', 3, 'dd6c37', 'c536d8c8-7'),
-('rpia6h', 'Valerie Fan', 'valerie93@example.com', 29, '38408f', 'cba55c06-2'),
-('ywr2sz', 'Yoland Deez', 'yolanda06@example.com', 7, '953efd', 'dc55eb42-0');
-
-
+('o5pllb', 'Brandon Campbell', 'campbellbrandon@example.net', 23, '0d4aa4', '1AAA'),
+('6wftsi', 'Jennifer Combs', 'combsjennifer@example.org', 24, 'ed9b4f', '1AAG'),
+('2aobyz', 'Donald Trump', 'donald13@example.com', 27, '148482', '1AAJ'),
+('fvsoti', 'Kristen Dougherty', 'doughertykristen@example.org', 10, '02ea11', '1AAR'),
+('na6etk', 'Francis Young', 'fyoung@example.org', 12, '6d4305', '2AAB'),
+('joer0v', 'Manuel Green', 'greenmanuel@example.org', 16, 'ceecd7', '1AAY'),
+('mubu6e', 'Joshua Jones', 'jonesjoshua@example.org', 1, 'e5a8fa', '2AAM'),
+('ohmynu', 'Lonnie Anderson', 'landerson@example.com', 25, 'c72f80', '1AAH'),
+('1b3f8x', 'Rhonda Livingston', 'livingstonrhonda@example.org', 20, '1fd3ee', '1AAQ'),
+('2ibrzw', 'Ryan Gordon', 'rgordon@example.net', 2, 'cfe942', '1AAS'),
+('qtq1da', 'Ronald Jacobson', 'rjacobson@example.com', 3, 'dd6c37', '2AAD'),
+('rpia6h', 'Valerie Fan', 'valerie93@example.com', 29, '38408f', '2AAE'),
+('ywr2sz', 'Yoland Deez', 'yolanda06@example.com', 7, '953efd', '1AAU');
 
 -- SYSTEM ADMIN ACCESS TABLES 
 
